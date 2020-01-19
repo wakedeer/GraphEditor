a06722ea9df3a44d734b3a68777a6f33f0fe3bc3
Aleksei Mironov <wakedeer@gmail.com>
2020-01-19 17:33:05 +0300

    refactoring checked

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ var nodesOne = svgOne.group();
 var svgTwo = SVG('stage-2').size("100%", 900);
 var nodesTwo = svgTwo.group();
 
-function recounterNodes(part, nodeNumber) {
+function recountNodes(part, nodeNumber) {
     $("#stage-" + part).find('.node-number').each(function () {
         let currentValue = $(this).html();
         if (nodeNumber < currentValue) {
@@ -77,79 +77,6 @@ function createNode(evt) {
     return node;
 }
 
-svgOne.dblclick(function (evt) {
-    let part = "1";
-
-    let node = createNode(evt);
-
-    let currentNodeNumber = ++counterOne;
-
-    drowNodeElements(node, currentNodeNumber);
-
-    node.on('contextmenu', function () {
-        let nodeNumber = $(this.node).find('text').html();
-        this.remove();
-        worksOne = $.grep(worksOne, removeNeighborsWorks(nodeNumber));
-
-        //removing countdown
-        worksOne.forEach(recalculateWorks(nodeNumber));
-        recounterNodes(part, nodeNumber);
-        counterOne--;
-    });
-
-    node.click(function () {
-        if (candidateNodeOne === this) {
-            this.children()[0].fill({color: CIRCLE_COLOR});
-            candidateNodeOne = null;
-        } else if (candidateNodeOne) {
-
-            let currentValue = this.children()[1].node.textContent;
-            let previousValue = candidateNodeOne.children()[1].node.textContent;
-
-            let works = worksOne;
-            let isAlreadyExist = findDublicate(works, previousValue, currentValue);
-
-            if (currentValue < previousValue) {
-                showAlert("Переход из большего в меньшее не возможен!", "alert-danger", "1");
-            } else if (isAlreadyExist) {
-                showAlert("Дублирующий переход из состояния " + previousValue + " в состояние " + currentValue + " не возможен!", "alert-danger", "1");
-            } else {
-                let connectable = candidateNodeOne.connectable({
-                    marker: 'default',
-                    targetAttach: 'perifery',
-                    sourceAttach: 'perifery',
-                    color: CONNECTOR_COLOR
-                }, this);
-
-                let connectorId = $(connectable.connector.node).attr("id");
-                //remove work
-                connectable.connector.attr("stroke-width", 1.5);
-
-                connectable.connector.on('contextmenu', function () {
-                    let connectorId = this.id();
-                    worksOne = $.grep(worksOne, function (work) {
-                        return work.id !== connectorId;
-                    });
-                    this.remove();
-                });
-                works.push(
-                    {
-                        id: connectorId,
-                        source: parseInt(previousValue, 10),
-                        target: parseInt(currentValue, 10)
-                    }
-                );
-                this.children()[0].fill({color: CIRCLE_COLOR});
-                candidateNodeOne.children()[0].fill({color: CIRCLE_COLOR});
-                candidateNodeOne = null;
-            }
-        } else {
-            candidateNodeOne = this;
-            candidateNodeOne.children()[0].fill({color: CANDIDATE_CIRCLE_COLOR})
-        }
-    });
-});
-
 function validation(counter, works, states, part) {
     for (let i = 0; i <= counter; i++) {
         let previous = [];
@@ -200,15 +127,6 @@ function validation(counter, works, states, part) {
     }
 }
 
-$("#validation-btn-1").click(function () {
-    let part = "1";
-    let counter = counterOne;
-    let works = worksOne;
-    statesOne = [];
-    let states = statesOne;
-    validation(counter, works, states, part);
-});
-
 function showAlert(text, type, part) {
     let alert = $("#message-alert-" + part);
     alert.removeClass("alert-danger");
@@ -225,10 +143,15 @@ function hideAlert(part) {
     alert.css("display", "none");
 }
 
-$("#alert-close-btn-1").click(function (e) {
-    let part = "1";
-    hideAlert(part);
-});
+function storeWorks(part) {
+    worksOne.forEach(function (work) {
+        let cell = $("#work-table-" + part).find('td[data-work-id="' + work.id + '"]');
+        let time = cell.html();
+        if (time) {
+            work.time = parseInt(time, 10);
+        }
+    });
+}
 
 function calculate(works, part, states) {
     for (let i = 0; i < works.length; i++) {
@@ -278,10 +201,10 @@ function calculate(works, part, states) {
         }
         states[i].r = states[i].tn - states[i].tp;
         if (states[i].r == 0) {
-            $("#stage-1").find("circle[data-node-num='" + i + "']").attr("fill", CRITICAL_PATH_CIRCLE_COLOR);
+            $("#stage-" + part).find("circle[data-node-num='" + i + "']").attr("fill", CRITICAL_PATH_CIRCLE_COLOR);
             msg = "-" + i + msg;
         } else {
-            $("#stage-1").find("circle[data-node-num='" + i + "']").attr("fill", CIRCLE_COLOR);
+            $("#stage-" + part).find("circle[data-node-num='" + i + "']").attr("fill", CIRCLE_COLOR);
         }
     }
 
@@ -291,25 +214,104 @@ function calculate(works, part, states) {
         statesTableHtml += '<tr><th scope="row">' + state.id + '</th><td>' + state.prev + '</td><td>' + state.next + '</td><td>' + state.tp + '</td><td>' + state.tn + '</td><td>' + state.r + '</td></tr>';
     });
 
-    $("#states-table-1").html(statesTableHtml);
+    $("#states-table-" + part).html(statesTableHtml);
 
-    showAlert("Критический путь " + msg + "  равен " + states[states.length - 1].tp, "alert-warning", "1")
+    showAlert("Критический путь " + msg + "  равен " + states[states.length - 1].tp, "alert-warning", part)
 }
+
+//         ----------------part sensitive functions
+
+svgOne.dblclick(function (evt) {
+    let part = "1";
+
+    let node = createNode(evt);
+
+    let currentNodeNumber = ++counterOne;
+
+    drowNodeElements(node, currentNodeNumber);
+
+    node.on('contextmenu', function () {
+        let nodeNumber = $(this.node).find('text').html();
+        this.remove();
+        worksOne = $.grep(worksOne, removeNeighborsWorks(nodeNumber));
+
+        //removing countdown
+        worksOne.forEach(recalculateWorks(nodeNumber));
+        recountNodes(part, nodeNumber);
+        counterOne--;
+    });
+
+    node.click(function () {
+        if (candidateNodeOne === this) {
+            this.children()[0].fill({color: CIRCLE_COLOR});
+            candidateNodeOne = null;
+        } else if (candidateNodeOne) {
+
+            let currentValue = this.children()[1].node.textContent;
+            let previousValue = candidateNodeOne.children()[1].node.textContent;
+
+            let works = worksOne;
+            let isAlreadyExist = findDublicate(works, previousValue, currentValue);
+
+            if (currentValue < previousValue) {
+                showAlert("Переход из большего в меньшее не возможен!", "alert-danger", part);
+            } else if (isAlreadyExist) {
+                showAlert("Дублирующий переход из состояния " + previousValue + " в состояние " + currentValue + " не возможен!", "alert-danger", part);
+            } else {
+                let connectable = candidateNodeOne.connectable({
+                    marker: 'default',
+                    targetAttach: 'perifery',
+                    sourceAttach: 'perifery',
+                    color: CONNECTOR_COLOR
+                }, this);
+
+                let connectorId = $(connectable.connector.node).attr("id");
+                //remove work
+                connectable.connector.attr("stroke-width", 1.5);
+
+                connectable.connector.on('contextmenu', function () {
+                    let connectorId = this.id();
+                    worksOne = $.grep(worksOne, function (work) {
+                        return work.id !== connectorId;
+                    });
+                    this.remove();
+                });
+                works.push(
+                    {
+                        id: connectorId,
+                        source: parseInt(previousValue, 10),
+                        target: parseInt(currentValue, 10)
+                    }
+                );
+                this.children()[0].fill({color: CIRCLE_COLOR});
+                candidateNodeOne.children()[0].fill({color: CIRCLE_COLOR});
+                candidateNodeOne = null;
+            }
+        } else {
+            candidateNodeOne = this;
+            candidateNodeOne.children()[0].fill({color: CANDIDATE_CIRCLE_COLOR})
+        }
+    });
+});
+
+$("#validation-btn-1").click(function () {
+    let part = "1";
+    let counter = counterOne;
+    let works = worksOne;
+    statesOne = [];
+    let states = statesOne;
+    validation(counter, works, states, part);
+});
+
+$("#alert-close-btn-1").click(function (e) {
+    let part = "1";
+    hideAlert(part);
+});
 
 $("#calculate-btn-1").click(function (e) {
     let part = "1", works = worksOne, states = statesOne;
     calculate(works, part, states);
 });
-
-function storeWorks(part) {
-    worksOne.forEach(function (work) {
-        let cell = $("#work-table-" + part).find('td[data-work-id="' + work.id + '"]');
-        let time = cell.html();
-        if (time) {
-            work.time = parseInt(time, 10);
-        }
-    });
-}
 
 $("#work-table-1").on('blur', "td[data-work-id]", function (e) {
     storeWorks("1");
